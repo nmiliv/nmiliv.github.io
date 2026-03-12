2b6d9c0b221b9656f2e1a3e6dd003defeddea5d0
Noa/h Milivojevic <noah@milivojevic.org>
2026-03-11 23:03:28 -0700

    footnotes work

--- traveling/printPlaceList.js
+++ traveling/printPlaceList.js
@@ -1,25 +1,21 @@
 // TODO favicon issue
-// TODO world map, or add coordinates somehow
-// TODO graph showing all places and tag connections somehow... sorta like a subway map of sorts? so parent inherits all tags of child, all the way back to root.
-// TODO do we want to make places collapsible? collapse or expand by default? Also buttons to collapse/expand all
+// TODO world map, or add coordinates somehow, or the date of writing or time of visit...
+//  graph showing all places and tag connections somehow... sorta like a subway map of sorts? so parent inherits all tags of child, all the way back to root.
 // note: if you are searching through tag classes, remember to replace whitespace with dashes
-// low hanging fruit: prettier comboboxes, text search --> text can wait till later. Comboboxes are more important at the moment, and probably the last thing needed before release
+// low hanging fruit: text search --> text can wait till later
 // next step: URLs and sharing
+// TODO footnotes and images
+// footnotes strategy: use spans with class footnotes, on load, pull internal text and replace with numbers and link, and generate footnote table at the bottom as usual. so like <span class="footnote">footnote content</span> diplays as <span class="headnote">[0]</span> with <li> at the bottom.
 
-// text search: find items matching search, add all parents of matches (minimal content), add all children of matches (collapsed... ugh that means adding collapsible content.) Actually no, just do the normal thing of minimize parents, show all children.
+// text search: find items matching search, add all parents of matches (minimal content), add all children of matches (collapsed... ugh that means adding collapsible content.) Actually no, just do the normal thing of minimize parents, but show all children unollapsed.
 
-const searchButton = document.getElementById("tagsearch-button");
 const searchClearButton = document.getElementById("tagsearch-clear");
-const searchBox = document.getElementById("tagsearch-text");
 let searchtaglist = [];
 function searchFunction(){
-    if (searchBox.value !== "") pushIfUnique(searchtaglist, searchBox.value.split(/\s*,\s*/));// searchtaglist.push(...searchBox.value.split(/\s*,\s*/)); // regex matches comma surrounded by whitespace
     searchtaglist = searchtaglist.filter((el) => alltags.some((item) => item == el)); // remove duplicates
     reloadTags(document.getElementById("searchtaglist"), searchtaglist);
     makePlaceList(placelist(), searchtaglist); // ok it seems like this placelist is the problem... need a new plan. Likely just have a seperate manager for adding/removing new places from myplacelist. Actually seems like it works fine, I just had something written weird before.
-    searchBox.value = "";
 }
-searchButton.addEventListener("click", () => searchFunction());
 searchClearButton.addEventListener("click", () => {
     searchtaglist = [];
     reloadTags(document.getElementById("searchtaglist"), searchtaglist);
@@ -32,19 +28,28 @@ window.onload = async function() {
     // TODO how to do the funny url thing for searching?
     // TODO non-tag search (bc I don't expecct people to ctrl-f) --> this is gonna need some more planning oh how this will work
     reloadTags(document.getElementById("taglist"), taglist, placelist());
+    loadSearchTags();
 }
 let myplacelist = [];
-
+const loadSearchTags = () => {
+    const newdiv = document.getElementById("searchtagoptions");
+    while (newdiv.firstChild) newdiv.removeChild(newdiv.lastChild);
+    for (const tag of alltags){
+        generateTag(tag, () => {
+            pushIfUnique(searchtaglist, tag);
+            searchFunction();
+        }, newdiv);
+    }
+};
+// to do finihs unfocus, can't do that now or else i'll miss my trains stop bc i'm too tired. I need to go out more often :)'
 
 function makePlaceList(placelist, displaytaglist = [], textsearch = []){
     const mydiv = document.getElementById("logs");
     const placesuggestions = document.getElementById("suggestions");
     const tagsuggestions = document.getElementById("tag-suggest");
     while (placesuggestions.firstChild) placesuggestions.removeChild(placesuggestions.lastChild);
-    // placesuggestions.innerHTML = "";
     while (tagsuggestions.firstChild) tagsuggestions.removeChild(tagsuggestions.lastChild);
-    // tagsuggestions.innerHTML = "";
-    tagslist = ["Restaurant", "Country", "State", "City", "Breakfast", "Lunch", "Dinner", "Hotel", "Park", "Next Time", "Transit"];
+    tagslist = ["Other Places"]; // Other Places will be the only default tag lol. For legacy reasons.
     // search objective: display places with certain tags, and display the full parent chain needed (though maybe ignore decriptions)
     const rootlist = [];
     const orphanlist = [];
@@ -60,7 +65,6 @@ function makePlaceList(placelist, displaytaglist = [], textsearch = []){
         }
     }
     let displaylist = placelist;
-    // if (displaytaglist.length !== 0 && !(displaytaglist.some((el) => el.toUpperCase() === "OTHER PLACES" || el.toUpperCase() === "NEXT TIME"))) {
     if (displaytaglist.length !== 0) {
         // if len=0, then only link a filtered view. If we're looking for a post-linking style place, then link the whole list
         // really, this sort of filtering from placelist to displaylist is optional, because places get filtered during printing anyway, but we still do some pre-filtering here for historical purposes. Anyhow, this comment is now longer than the code it is supposed to describe and there isn't really that much interesitng going on here, so I'll stop yapping.
@@ -97,7 +101,6 @@ function makePlaceList(placelist, displaytaglist = [], textsearch = []){
         const newsuggestion = document.createElement("option");
         newsuggestion.value = place.name;
         placesuggestions.appendChild(newsuggestion);
-        // placesuggestions.innerHTML += "<option value=\"" + place.name + "\">";
         if ('taglist' in place){
             for (tag of place.taglist) {
                 if (!(tagslist.includes(tag))) {
@@ -110,15 +113,6 @@ function makePlaceList(placelist, displaytaglist = [], textsearch = []){
         const newsuggestion = document.createElement("option");
         newsuggestion.value = tag;
         tagsuggestions.appendChild(newsuggestion);
-        // tagsuggestions.innerHTML += "<option value=\"" + tag + "\">";
-        // const newtag = document.createElement("li"); // holdover code from w3 ARIA combobox
-        // newtag.appendChild(document.createTextNode(tag));
-        // newtag.id = "lb1-" + tag;
-        // newtag.role = "option";
-        // tagsuggestions.appendChild(newtag);
-        // console.log("hi i am trying to add list item");
-        // console.log(newtag);
-        // tagsuggestions.innerHTML += "<li id=\"lb1-" + tag + "\" role=\"option\">" + tag + "</li>";
     }
     function printPlace(place, container, level=0, special=""){
         // if place has no children and does not contain search criteria, don't print it
@@ -152,12 +146,14 @@ function makePlaceList(placelist, displaytaglist = [], textsearch = []){
                         pushIfUnique(searchtaglist, tag);
                         searchFunction();
                     }, newdiv);
-                    if (tag !== place.taglist.at(-1)) newdiv.appendChild(document.createTextNode(", "));
                 }
                 newdiv.appendChild(document.createElement("br"));
             }
             if (place.content !== "") {
-                newdiv.appendChild(document.createTextNode(place.content));
+                // newdiv.appendChild(document.createTextNode(place.content));
+                const textdiv = document.createElement("div");
+                textdiv.innerHTML = place.content; // we use innerHTML magic here to preserve HTML magic in footnote generation and such.
+                newdiv.appendChild(textdiv);
             }
         }
         container.appendChild(newdiv);
@@ -175,7 +171,6 @@ function makePlaceList(placelist, displaytaglist = [], textsearch = []){
                             pushIfUnique(searchtaglist, tag);
                             searchFunction();
                         }, li);
-                        if (tag !== otherplace.taglist.at(-1)) newdiv.appendChild(document.createTextNode(", ")); // TODO also make tag search prettier.
                     }
                     li.appendChild(document.createTextNode(")"));
                 }
@@ -193,25 +188,30 @@ function makePlaceList(placelist, displaytaglist = [], textsearch = []){
             }
         } else if (special === "other"){ // well I mean technically this information gets carried by the name, but also I can't be bothered right now
             printListingSpecial("These are places that I don’t have stories about, but that I otherwise think are alright.");
+            // other places must be checked whether any object in placelist qualifies for an other-places tag regardless of if it will be displayed
         } else if (special === "nexttime"){
             printListingSpecial("These are places that I haven't been to, but I think might be cool.");
+            // next time tag will always be pushed by the linker, since next time is specified at object creation
         } else {
             if (document.getElementById('generate')) alert("Received special that doesn't exist\nitem name: " + place.name + "\nspecial type: " + special + "\nThis message should only appear on development pages, if you are not Noa/h then something has gone wrong (maybe send Noa/h an email about this?)");
         }// if you are here to add more specials options, and the special displays children, make sure the div is properly classed! (See above).
     }
+    function sortAlphaSpecial(a,b) {
+        if (typeof a === 'string') a = {name: a};
+        if (typeof b === 'string') b = {name: b};
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+        if (nameA.includes("TRANSIT") || nameB === "NEXT TIME") return -1; // ensure transit comes first and other places comes last
+        if (nameB.includes("TRANSIT") || nameA === "NEXT TIME") return  1;
+        if (nameA === "OTHER PLACES") return  1; // neither name is transit or next time, or we would have hit one of the previosu ifs
+        if (nameB === "OTHER PLACES") return -1;
+        if (nameA < nameB) return -1; // if not a special case, then regular sort
+        if (nameA > nameB) return  1;
+        return 0;
+    }
     function printPlaceList(list, container, level=0){
         // ok so what if... as we go through the list, grab places with no content and no children, don't print them, and instead construct an other places object, and then print that
-        list.sort((a,b) => {
-            const nameA = a.name.toUpperCase();
-            const nameB = b.name.toUpperCase();
-            if (nameA.includes("TRANSIT") || nameB === "NEXT TIME") return -1; // ensure transit comes first and other places comes last
-            if (nameB.includes("TRANSIT") || nameA === "NEXT TIME") return  1;
-            if (nameA === "OTHER PLACES") return  1; // neither name is transit or next time, or we would have hit one of the previosu ifs
-            if (nameB === "OTHER PLACES") return -1;
-            if (nameA < nameB) return -1; // if not a special case, then regular sort
-            if (nameA > nameB) return  1;
-            return 0;
-        });
+        list.sort((a,b) => sortAlphaSpecial(a,b));
         let otherplace = { child: [],}; // bodge so that we define otherplaces
         let nexttime = { child: [],};
         if (list.length !== 0){
@@ -292,11 +292,81 @@ function makePlaceList(placelist, displaytaglist = [], textsearch = []){
         // mydiv.innerHTML += "<h2>orphans</h2>"
         printPlaceList(orphanlist, mydiv)
     }
+    for (const place of placelist) {
+        if ('taglist' in place) {
+            for (const tag of place.taglist) pushIfUnique(tagslist, tag);
+        }
+    }
+    alltags = tagslist;
+    alltags.sort((a,b) => sortAlphaSpecial(a,b));
     const cssStyle = document.createElement('style');
     cssStyle.type = 'text/css';
-    for (let i = 0; i < tagslist.length; i++) cssStyle.appendChild(document.createTextNode(".tag-" + tagslist[i].replace(" ", "-") + "{background-color: hsl(" + i/tagslist.length*360 + "deg 60% 40%);}"));
+    for (let i = 0; i < alltags.length; i++) cssStyle.appendChild(document.createTextNode(".tag-" + alltags[i].replace(" ", "-") + "{background-color: hsl(" + i/alltags.length*360 + "deg 60% 40%);}"));
     document.getElementsByTagName("head")[0].appendChild(cssStyle);
-    alltags = tagslist;
+    loadSearchTags();
+
+    // ok and now for footnotes...
+    const footnotes = document.getElementsByClassName("footnote");
+    const footlist = document.getElementById("footnotes");
+    while (footlist.firstChild) footlist.removeChild(footlist.lastChild);
+    const tooltipContainer = document.querySelector(".tooltip-container"); // TODO why is this a query selector??
+
+    var newlist = "";
+    var counter = 1;
+    // yes I made a whole system for re-arranging footnotes and making sure they are correctly numbered. Is there a library for this? Probably. Unfortunately I am an engineer and like solving problems that didn't need a new solution.
+    for (const headnote of footnotes) {
+        headnote.id = "headnote-" + counter;
+        const foottext = headnote.innerHTML;
+        const footnote = document.createElement("li");
+        footnote.id = "footnote-" + counter;
+        footnote.innerHTML = foottext;
+        // replace internal footnote text
+        headnote.innerHTML = "[<a href=\"#" + footnote.id + "\" data-toggle=\"tooltip\">" + counter + "</a>]";
+        // add footnote to the list
+        footlist.appendChild(footnote);
+        // link everything
+        headnote.addEventListener("mouseenter", (e) => {
+            if (e.pageX < getWidth()/2){
+                tooltipContainer.style.left = `${e.pageX}px`;
+            } else {
+                tooltipContainer.style.left = `${e.pageX - getWidth()*0.25}px`;
+            }
+            tooltipContainer.style.top = `${e.pageY}px`;
+            tooltipContainer.innerHTML = footnote.innerHTML;
+            tooltipContainer.style.display = 'block';
+            tooltipContainer.style.opacity = 1;
+        });
+        headnote.addEventListener("mouseout", (e) => {
+            tooltipContainer.style.display = 'none';
+            tooltipContainer.style.opacity = 0;
+        });
+        footnote.innerHTML = "<a href=\"#" + headnote.id + "\">" + "^</a> " + footnote.innerHTML;
+        newlist += footnote.outerHTML + "\n";
+        counter++;
+    }
+    tooltipContainer.addEventListener('mouseenter', () => {
+
+        tooltipContainer.style.display = 'block';
+        tooltipContainer.style.opacity = 1;
+
+    })
+    tooltipContainer.addEventListener('mouseout', () => {
+
+        tooltipContainer.style.display = 'none';
+        tooltipContainer.style.opacity = 0;
+
+    })
+    if (!footlist.firstChild) footlist.appendChild(document.createTextNode("no footnotes :)"));
+}
+
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
 }
 
 function generateTag(buttonText, callback, parent){
@@ -316,7 +386,6 @@ function generateTag(buttonText, callback, parent){
 function clearPlaceList() {
     const mydiv = document.getElementById("logs");
     while(mydiv.firstChild) mydiv.removeChild(mydiv.lastChild);
-    // mydiv.innerHTML = "";
 }
 async function getTextFile(str) {
     let responsetext = "";
@@ -351,11 +420,11 @@ const reloadTags = (tagsdiv, ftaglist) => {
     while (tagsdiv.firstChild) tagsdiv.removeChild(tagsdiv.lastChild);
     for (const item of ftaglist) {
         const newdiv = document.createElement("div");
-        tagsdiv.appendChild(newdiv);
+        // tagsdiv.appendChild(newdiv);
         generateTag(item, () => {
             ftaglist.splice(ftaglist.indexOf(item), 1);
             reloadTags(tagsdiv, ftaglist);
             makePlaceList(placelist(), searchtaglist);
-        }, newdiv); // are we missing a reload place list here as well? --> probably not, as reloadtags is usually called alongside a makeplacelist (especially now with dtandardized tag creation)
+        }, tagsdiv); // are we missing a reload place list here as well? --> probably not, as reloadtags is usually called alongside a makeplacelist (especially now with dtandardized tag creation)
     }
 }
